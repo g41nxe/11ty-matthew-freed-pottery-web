d4683ea96fddc8c8434b2485874f34787d23cc0f
Dan Hachenberger <g41nxe@gmail.com>
2020-10-20 22:09:54 -0700

    add tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
+var tailwindcss = require('tailwindcss')
+var autoprefixer = require('autoprefixer')
+
 module.exports = ({
     plugins: {
+        tailwindcss,
         autoprefixer: { 
             grid: 'autoplace'
         },
-        cssnano: {
-            preset: 'default'
-        }
     }
 })
