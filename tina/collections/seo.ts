6f3191d147291cd5eb4854dc1cbd282ad9eae6f3
Dan Hachenberger <g41nxe@gmail.com>
2026-07-19 14:55:07 +0200

    feat(tina): real seo collection schema

--- tina/collections/seo.ts
+++ tina/collections/seo.ts
@@ -7,5 +7,15 @@ export const seo: Collection = {
   format: "json",
   match: { include: "seo" },
   ui: { allowedActions: { create: false, delete: false } },
-  fields: [{ type: "string", name: "__placeholder", label: "placeholder" }],
+  fields: [
+    { type: "string", name: "title", label: "Title" },
+    { type: "string", name: "description", label: "Description", ui: { component: "textarea" } },
+    { type: "string", name: "url", label: "URL" },
+    { type: "string", name: "author", label: "Author" },
+    { type: "image", name: "image", label: "Image" },
+    {
+      type: "object", name: "options", label: "Options",
+      fields: [{ type: "string", name: "titleDivider", label: "Title Divider" }],
+    },
+  ],
 };
